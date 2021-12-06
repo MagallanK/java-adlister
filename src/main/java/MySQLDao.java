29d95219def39dab9492a5146d96a3c232f2dea0
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-12-06 16:13:49 -0600

    completed 3 for jdbc exercise

--- src/main/java/MySQLDao.java
+++ src/main/java/MySQLDao.java
@@ -1,12 +1,21 @@
+import com.mysql.cj.jdbc.Driver;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class MySQLDao implements Ads{
 
     private Connection connection;
+    private Config config = new Config();
 
-    public MySQLDao(Connection connection) {
-        this.connection = connection;
+    public MySQLDao(Connection connection) throws SQLException {new Driver();
+        this.connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUsername(),
+                config.getPassword()
+        );
     }
 
     @Override
